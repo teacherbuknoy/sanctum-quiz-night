819f0b459742e39a2dd1b1584ab774b790105591
Francis Rubio <francis.rubio@dice205.com>
2024-07-16 10:11:57 +0800

    Fix errors

--- src/assets/scripts/oda.js
+++ src/assets/scripts/oda.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const settings = document.createElement('script')
-const sdk = document.createElement('script')
+const websdk = document.createElement('script')
 
 settings.src = 'https://sanctum.francisrub.io/assets/images/settings.js'
 websdk.src = 'https://sanctum.francisrub.io/assets/images/websdk.js'
